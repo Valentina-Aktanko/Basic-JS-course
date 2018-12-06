82db54ea6c0808ba3436012daf6bf93e95423413
Valentina Aktanko <35582392+Valentina-Aktanko@users.noreply.github.com>
2018-12-06 22:21:34 +1000

    chess field with column names

--- lesson5/js1.js
+++ lesson5/js1.js
@@ -1,2 +1,37 @@
 'use strict';
 
+const chess = {
+    /**
+     * @property {HTMLElement} gameContainerElement Контейнер игры (DOM элемент).
+     */
+    gameContainerElement: document.getElementById('game'),
+
+
+
+    /**
+     * Метод отображения карты (игрового поля)
+     */
+    renderMap() {
+        // нахвания колонок
+        const colsName = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 0];
+
+        for (let row = 0; row < 10; row++) {
+            const trElem = document.createElement('tr');
+            this.gameContainerElement.appendChild(trElem);
+
+            for (let col = 0; col < 10; col++) {
+                const cell = document.createElement('td');
+                trElem.appendChild(cell);
+
+                if ((row === 0 || row === 9) && colsName[col] !== 0){
+                    cell.innerHTML = colsName[col];
+                }
+            }
+
+        };
+    },
+
+    isCellIsBlack() {},
+};
+
+chess.renderMap();
